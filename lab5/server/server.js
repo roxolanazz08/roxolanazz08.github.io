e450b4efe34fd63ec7be95c14a41fc4fc0f7e127
roxolanazz08 <roksolana.zaikina.oi.2024@lpnu.ua>
2026-03-30 19:43:18 +0300

    Fix path-to-regexp error with slash

--- lab5/server/server.js
+++ lab5/server/server.js
@@ -55,7 +55,7 @@ app.post("/api/reviews", async (req, res) => {
 });
 
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
